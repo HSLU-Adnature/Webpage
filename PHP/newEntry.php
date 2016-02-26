f9cfa4ab1d2b2e79b2c586f5f883efcff05d600b
Daniel Eisner <daniel.eisner@gmx.ch>
2016-02-26 14:27:47 +0100

    php load xml fixed

--- PHP/newEntry.php
+++ PHP/newEntry.php
@@ -1,9 +1,10 @@
 <?php
 
-$xml = simplexml_load_file("../core.xml");
-
-echo $xml->asXML();
-
-foreach ($xml->xpath('//title/text()') as $character) {
-    echo $character;
+if (file_exists('../xml/core.xml')) {
+    $xml = simplexml_load_file('../xml/core.xml');
+    foreach ($xml->event as $event) {
+        echo($event->title);
+    }
+} else {
+    exit('Konnte core.xml nicht öffnen.');
 }
