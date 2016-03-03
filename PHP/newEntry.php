d8e1f2d4b235e1897a81adab0f45431aaa5dcbfd
FBoss <flavio.boss@stud.hslu.ch>
2016-03-03 12:13:18 +0100

    First (incomplete) Backend implementation

--- PHP/newEntry.php
+++ PHP/newEntry.php
@@ -1,10 +1,34 @@
 <?php
 
-if (file_exists('../xml/core.xml')) {
-    $xml = simplexml_load_file('../xml/core.xml');
-    foreach ($xml->event as $event) {
-        echo($event->title);
-    }
-} else {
-    exit('Konnte core.xml nicht öffnen.');
-}
+$xml = simplexml_load_file('../xml/core.xml');
+
+$xml->registerXPathNamespace("ad", 'http://www.adnature.ch/core');
+
+$adnature_events = $xml->xpath("//ad:adnature_events")[0];
+
+$event = $adnature_events->addChild("event");
+$event->addChild("title", $_POST["title"]);
+$event->addChild("start_time", $_POST["start_time"]);
+$event->addChild("end_time", $_POST["end_time"]);
+$event->addChild("address", $_POST["address"]);
+$event->addChild("zip", $_POST["zip"]);
+$event->addChild("city", $_POST["city"]);
+$event->addChild("location", $_POST["location"]);
+$event->addChild("short_description", $_POST["short_description"]);
+$event->addChild("description", $_POST["description"]);
+
+$owner = $event->addChild("owner");
+$owner->addChild("surname", $_POST["surname"]);
+$owner->addChild("lastname", $_POST["lastname"]);
+$owner->addChild("email", $_POST["email"]);
+$owner->addChild("number", $_POST["number"]);
+
+$event->addChild("homepage", $_POST["homepage"]);
+$event->addChild("type", $_POST["type"]);
+$event->addChild("picture", $_POST["picture"]);
+$event->addChild("timestamp", time());
+
+$xml->saveXML();
+
+
+echo $adnature_events->asXML();
