7f2bcc6c75f9619a2b8f0b08fdeea71414b51378
Daniel Eisner <daniel.eisner@gmx.ch>
2016-03-10 19:25:20 +0100

    little bug fixed

--- PHP/planer.php
+++ PHP/planer.php
@@ -1,25 +1,46 @@
 <?php
 
-    $chosenId = $_GET['chosen'];
-    if (!isset($chosenId)) {
-        $chosenId = 1;
-    }
+$chosenId = $_GET['chosen'];
+if (!isset($chosenId)) {
+    $chosenId = 1;
+}
 
-    $xmlDoc = new DOMDocument();
-    $xmlDoc->load("../xml/core.xml");
+$xmlDoc = simplexml_load_file("../xml/core.xml");
+$xmlDoc->registerXPathNamespace("ad", 'http://www.adnature.ch/core');
 
-    $xmlEmpty = new DOMDocument();
-    $xmlEmpty->load("../xml/core_empty.xml");
+$xmlEmpty = simplexml_load_file("../xml/core_empty.xml");
+$xmlEmpty->registerXPathNamespace("ad", 'http://www.adnature.ch/core');
 
-    $xpath = new DOMXPath($xmlDoc);
-    $chosenEvent = $xpath->query("//event[id = $chosenId]");
-    echo $chosenEvent->length;
-    $xmlEmpty->appendChild($chosenEvent->item(0));
+$chosenEvent = $xmlDoc->xpath("//ad:event[ad:id=$chosenId]");
+$adnature_events = $xmlEmpty->xpath("//ad:adnature_events")[0];
+$event = $adnature_events->addChild("event");
+$event->addChild("id", $chosenEvent[0]->id);
+$event->addChild("title", $chosenEvent[0]->title);
+$event->addChild("date", $chosenEvent[0]->date);
+$event->addChild("start_time", $chosenEvent[0]->start_time);
+$event->addChild("end_time", $chosenEvent[0]->end_time);
+$event->addChild("address", $chosenEvent[0]->address);
+$event->addChild("zip", $chosenEvent[0]->zip);
+$event->addChild("city", $chosenEvent[0]->city);
+$event->addChild("location", $chosenEvent[0]->location);
+$event->addChild("short_description", $chosenEvent[0]->short_description);
+$event->addChild("description", $chosenEvent[0]->description);
+$owner = $event->addChild("owner");
+$owner->addChild("firstname", $chosenEvent[0]->owner->firstname);
+$owner->addChild("surname", $chosenEvent[0]->owner->surname);
+$owner->addChild("email", $chosenEvent[0]->owner->email);
+$owner->addChild("number", $chosenEvent[0]->owner->number);
+$event->addChild("homepage", $chosenEvent[0]->homepage);
+$event->addChild("type", $chosenEvent[0]->type);
+$event->addChild("picture", $chosenEvent[0]->picture);
+$event->addChild("timestamp", $chosenEvent[0]->timestamp);
 
-    $xslDoc = new DOMDocument();
-    $xslDoc->load("../xml/event.xsl");
 
-    $proc = new XSLTProcessor();
-    $proc->importStylesheet($xslDoc);
-    echo $proc->transformToXML($xmlEmpty);
-?>
+$xmlEmpty->saveXML("../xml/core_empty.xml");
+
+$xslDoc = new DOMDocument();
+$xslDoc->load("../xml/event.xsl");
+
+$proc = new XSLTProcessor();
+$proc->importStylesheet($xslDoc);
+echo $proc->transformToXML($xmlEmpty);
