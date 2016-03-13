541e4c0c57182fac2a194b9a248c47165698e558
Lötscher Dario tmloetsc <dario.loetscher@stud.hslu.ch>
2016-03-13 19:55:56 +0100

    made maps.xml useless - geo.php fetches data directly from Google Maps

--- PHP/geo.php
+++ PHP/geo.php
@@ -1,4 +1,21 @@
 <?php
+
+function urlencodeEvent($event){
+    $eventEnc = new stdClass();
+    $eventEnc->address = urlencode($event->address);
+    $eventEnc->zip = $event->zip;
+    $eventEnc->city = urlencode($event->city);
+    return $eventEnc;
+}
+
+function getGeoLocation($event){
+    $eventUrl = urlencodeEvent($event);
+    $data = file_get_contents
+    ("https://maps.googleapis.com/maps/api/geocode/xml?address=$eventUrl->address+$eventUrl->zip+$eventUrl->city&key=AIzaSyCtxiB_gzEsuW95lH1xY5zo3Nre1XKhQUE");
+    $googleXmlAnswer = simplexml_load_string($data);
+    return $googleXmlAnswer->xpath("//geometry/location")[0];
+}
+
 if(!isset($_GET["events"])){
     echo "no events";
     exit();
@@ -6,16 +23,18 @@ if(!isset($_GET["events"])){
 
 $eventIds = explode(",", $_GET["events"]);
 
-$xmlDoc = simplexml_load_file("../xml/maps.xml");
+$xmlDoc = simplexml_load_file("../xml/core.xml");
+$xmlDoc->registerXPathNamespace("ad", 'http://www.adnature.ch/core');
 
 $xmlGeoMarks = new SimpleXMLElement("<marks></marks>");
 foreach($eventIds as $id){
-    $eventLocation = $xmlDoc->xpath("//event[id=$id]/GeocodeResponse/result/geometry/location")[0];
+    $event = $xmlDoc->xpath("//ad:event[ad:id=$id]")[0];
+    $eventLocation = getGeoLocation($event);
     $eventGeo = $xmlGeoMarks->addChild("event");
     $eventGeo->addChild("id", $id);
     $eventGeoLocation = $eventGeo->addChild("location");
-    $eventGeoLocation->addChild("lat", (string) $eventLocation->lat);
-    $eventGeoLocation->addChild("lng", (string) $eventLocation->lng);
+    $eventGeoLocation->addChild("lat", $eventLocation->lat);
+    $eventGeoLocation->addChild("lng", $eventLocation->lng);
 }
 header('Content-Type: application/xml');
 echo $xmlGeoMarks->asXML();
