0648e207620ab3217c23fd1efb90290ad8eb25de
FBoss <flavio.boss@stud.hslu.ch>
2016-03-14 09:13:33 +0100

    Filter by EventID added

--- PHP/detail.php
+++ PHP/detail.php
@@ -1,10 +1,22 @@
 <?php
 
 ini_set('display_errors', 1);
+
+$filename = '../xml/core.xml';
+$xml_file = simplexml_load_file($filename);
+
 $xml = new DOMDocument();
-$xml->load("../xml/core.xml");
+$xml = dom_import_simplexml($xml_file)->ownerDocument;
 
 if ($xml->schemaValidate("../xml/core.xsd")) {
+
+    $xml_file->registerXPathNamespace("ad", 'http://www.adnature.ch/core');
+
+    if (isset($_POST["id"])) {
+        $adnature_events = $xml->xpath("//ad:event[ad:id=" . $_POST["id"] . "]");
+    } else {
+        $adnature_events = $xml;
+    }
 
     $xsl = new DOMDocument;
     $xsl->load('../xml/event_detail.xsl');
@@ -12,7 +24,7 @@ if ($xml->schemaValidate("../xml/core.xsd")) {
     $proc = new XSLTProcessor;
     $proc->importStyleSheet($xsl);
 
-    echo $proc->transformToXML($xml);
+    echo $proc->transformToXML($adnature_events);
 
 } else {
     echo "XML not valid!";
