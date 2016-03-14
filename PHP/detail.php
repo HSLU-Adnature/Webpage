70870aa2350c4fc2ae85df651ecbb02998c0698b
FBoss <flavio.boss@stud.hslu.ch>
2016-03-14 10:29:00 +0100

    Changed simplexml commands to DOM

--- PHP/detail.php
+++ PHP/detail.php
@@ -3,20 +3,22 @@
 ini_set('display_errors', 1);
 
 $filename = '../xml/core.xml';
-$xml_file = simplexml_load_file($filename);
 
 $xml = new DOMDocument();
-$xml = dom_import_simplexml($xml_file)->ownerDocument;
+$xml->load($filename);
 
 if ($xml->schemaValidate("../xml/core.xsd")) {
 
-    $xml_file->registerXPathNamespace("ad", 'http://www.adnature.ch/core');
+    $xpath = new DOMXPath($xml);
+    $xpath->registerNamespace("ad", 'http://www.adnature.ch/core');
 
     if (isset($_GET["id"])) {
-        $adnature_events = $xml_file->xpath("//ad:event[ad:id=" . htmlspecialchars($_GET["id"]) . "]");
+        $query = "//ad:event[ad:id=" . htmlspecialchars($_GET["id"]) . "]";
     } else {
-        $adnature_events = $xml_file->xpath("//ad:event");
+        $query = "//ad:event";
     }
+
+    $result = $xpath->query($query, $xml);
 
     $xsl = new DOMDocument;
     $xsl->load('../xml/event_detail.xsl');
@@ -24,7 +26,7 @@ if ($xml->schemaValidate("../xml/core.xsd")) {
     $proc = new XSLTProcessor;
     $proc->importStyleSheet($xsl);
 
-    echo $proc->transformToXML(dom_import_simplexml($adnature_events[0])->ownerDocument);
+    echo $proc->transformToXML($result[0]);
 
 } else {
     echo "XML not valid!";
